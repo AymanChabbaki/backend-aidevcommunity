471c8b53610a25b711996e1e0fb0f64481d4fd7b
AymanChabbaki <ayman.chabbaki@arkx.academy>
2025-11-29 22:32:08 +0100

    fin

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ app.use(helmet({
   crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:8081', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:8081', 'http://localhost:3000','https://frontend-aidevcommunity.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
